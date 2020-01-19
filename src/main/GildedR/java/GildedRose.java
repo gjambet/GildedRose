4e37fd57057e78ac7ceaad82bfe349c76ba9e8f7
Keriche <kevin.keriche@etu.univ-smb.fr>
2020-01-19 23:51:34 +0100

    Tests + refractoring

---
+++ src/main/GildedR/java/GildedRose.java
@@ -0,0 +1,50 @@
+import java.util.Optional;
+
+
+public class GildedRose {
+
+    private StockofItems stkItems;
+    private QualityItemsManager Qim;
+
+    public StockofItems getGoods() {
+        return this.stkItems;
+    }
+
+    public QualityItemsManager getQIM() {
+        return Qim;
+    }
+
+    public GildedRose() {
+
+        this.stkItems = new StockofItems();
+        this.Qim = new QualityItemsManager();
+    }
+
+    public Item get(String itemName) {
+        StockofItems StockofItems = new StockofItems();
+        Optional<Item> optional =
+                StockofItems.getItems().stream()
+                        .filter(item -> item.getName().equals(itemName))
+                        .findFirst();
+
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            throw new IllegalStateException("pas trouvé");
+        }
+
+
+    }
+
+    public void updateQuality() {
+
+        this.stkItems.getItems().stream()
+                .forEach(x -> {
+                    Qim.updateQualityItem(x);
+                });
+    }
+
+    public void updateQuality(Item item) {
+        Qim.updateQualityItem(item);
+    }
+}
